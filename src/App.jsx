75c44895562e171c105a7369b4c2573db4b9768c
Sérgio Henrique <sergiohenriqueqqq@gmail.com>
2025-04-07 19:45:08 -0300

    acertando a estrutura do projeto
    tinha uma pasta a mais e dois readme, ajeitei isso

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
-    <div>     
-      <Router>   
+    <div>
+      <Router>
         <Routes>
-          
+
         </Routes>
-      </Router>     
+      </Router>
     </div>);
 }
 
